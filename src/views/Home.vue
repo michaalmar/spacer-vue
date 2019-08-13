99e02a8c9498d65a54bf4efcfc431428f63ac3c4
michaalmar <michalmarchela@gmail.com>
2019-08-13 14:02:26 +0200

    search

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,70 @@
 <template>
-  <div class="home">
-    <h1>SPACER</h1>
+  <div class="wrapper">
+    <div class="search">
+      <label for="search">Search</label>
+      <form v-on:submit.prevent="getResult(searchValue)">
+        <input id="search" name="search" v-model="searchValue" />
+      </form>
+
+      <ul>
+        <li v-for="item in results" :key="item.data[0].nasa_id">
+          <p>{{item.data[0].description}}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 
 export default {
-  name: 'Home'
+  name: "Home",
+  data() {
+    return {
+      searchValue: "",
+      results: []
+    };
+  },
+  methods: {
+    getResult(searchValue) {
+      axios
+        .get(
+          "https://images-api.nasa.gov/search?q=" +
+            searchValue +
+            "&media_type=image"
+        )
+        .then(response => {
+          this.results = response.data.collection.items;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+.wrapper {
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.search {
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  font-size: 20px;
+
+  input {
+    height: 30px;
+    border: 0;
+    border-bottom: 1px solid black;
+    font-size: 20px;
+  }
+}
+</style>
